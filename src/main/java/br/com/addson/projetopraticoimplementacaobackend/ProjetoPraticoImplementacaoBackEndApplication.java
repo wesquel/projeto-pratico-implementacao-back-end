fd48944571c8fc6cbfbdd037463f72f1e1b3a233
Weslley Addson <wesquel@hotmail.com>
2025-03-28 21:41:35 -0300

    init project

---
+++ src/main/java/br/com/addson/projetopraticoimplementacaobackend/ProjetoPraticoImplementacaoBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.addson.projetopraticoimplementacaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPraticoImplementacaoBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoPraticoImplementacaoBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/addson/projetopraticoimplementacaobackend/ProjetoPraticoImplementacaoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.addson.projetopraticoimplementacaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPraticoImplementacaoBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
